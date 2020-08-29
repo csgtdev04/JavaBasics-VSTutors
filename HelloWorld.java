d78f38abc3eb30e0a5cac4d5d8f09ee088df4ea1
csgtdev04 <62195113+csgtdev04@users.noreply.github.com>
2020-08-29 14:21:15 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+public class HelloWorld {
+
+    public static void main(String[]args) {
+        //Write code
+
+        //Printing
+        System.out.println("My name is Sai");
+       
+        //Variables
+        int age = 10;
+        String name = "Sai";
+        
+        //integer is a number
+        System.out.println(name);
+
+        //Concatenation
+        System.out.println("My name is Sai and I am 17");
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+
+        //Add/Sub
+        System.out.println(2 / 3);
+        
+    }
+}
