067f21bbccd26211ae5f5251bbac380fadcb147c
csgtdev04 <saitanujm@gmail.com>
2020-10-10 19:10:23 -0500

    arraylist generics

---
+++ NinthClass.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+public class NinthClass {
+    public static void main(String[] args) {
+        //Review ArrayLists (10 mins)
+        //Generics
+        //int a => primitive type ("wrapper class - Integer")
+        //boolean a => primitive type ("wrapper class - Boolean")
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        //list.add("Hi");
+        //list.add("Hi1");
+        //list.add("Hi2");
+        //list.add("Hi3");
+        list.add(4);
+        list.add(1000000); //.add is known as a method
+        list.add(40); 
+        list.add(1, 50);
+        list.add(2, 3);
+        System.out.println(list.get(3)); //.get() is known as a method
+        System.out.println(list.size()); //.size() is known as a method
+        System.out.println(list);
+        //.add(number), .add(index, number), .size(), .get()
+        
+        //You create a list of groceries (you must have 5 items) and you need to show the size of the list and also add in another item you forgot to at index position 3
+        //You create a list of Booleans (you must have at least 3) and you need to tell me how many of them are false and how many are true, FOR LOOP
+
+       
+        //New students view presentation and look over GitHub (via website) while I teach Dhak/Latt
+    }
+}
+
+
+
+
+
+
+
+
