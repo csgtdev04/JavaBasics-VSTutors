e35d58e3d292b9faed449347173f630b19d985f8
csgtdev04 <saitanujm@gmail.com>
2021-01-09 15:29:27 -0600

    scanner implementation in person class

--- OOPs/Person.java
+++ OOPs/Person.java
@@ -1,36 +1,44 @@
 package OOPs;
 
-import java.util.List;
 
 public class Person {
     // Attributes
     String name;
     int age;
-    String[] candy_list;
+    // String[] candy_list;
 
     // Constructor
     public Person(String user_name, int user_age) {
         this.name = user_name;
         this.age = user_age;
-        this.candy_list = new String[2];
+        // this.candy_list = new String[2];
     }
 
     // Methods/Functions
-    public void eat() {
-        System.out.println(this.name + "is eating!");
+    public void eat() { 
+        System.out.println(this.name + " is eating!");
         //Random stuff
     }
 
-    public void createCandyList() {
-        this.candy_list[0] = "Snickers";
-        this.candy_list[1] = "M&Ms";
-    }
+    // public void createCandyList() {
+    //     this.candy_list[0] = "Snickers";
+    //     this.candy_list[1] = "M&Ms";
+    // }
 
+    //Getters
     public int getAge() {
         return age;
     }
 
     public String getName() {
         return name;
+    }
+
+    //Setters (void)
+    public void setName(String updated_name) {
+        this.name = updated_name;
+    }
+    public void setAge(int updated_age) {
+        this.age = updated_age;
     }
 }
--- OOPs/PersonRunner.java
+++ OOPs/PersonRunner.java
@@ -1,32 +1,45 @@
 package OOPs;
 
+import java.util.Scanner;
+
 public class PersonRunner {
     public static void main(String[] args) {
-        //We are going to instantiate our Person
-        //Scanner key = new Scanner(System.in);
-        Person person1 = new Person("Sai", 17);
-        Person person2 = new Person("Lattika", 12);
-        Person person3 = new Person("Dhakshesh", 15);
+        // We are going to instantiate our Person
+        Scanner key = new Scanner(System.in);
+        System.out.println("Whats you name?");
+        String person1_name = key.nextLine();
+
+        System.out.println("Whats you age?");
+        int person1_age = key.nextInt();
+        key.nextLine();
+
+        Person person1 = new Person(person1_name, person1_age);
+
+        // // Person person2 = new Person("Lattika", 12);
+        // // Person person3 = new Person("Dhakshesh", 15);
 
         int age1 = person1.getAge();
         String name1 = person1.getName();
-        
-        int age2 = person2.getAge();
-        String name2 = person2.getName();
+        System.out.println(age1 + name1);
 
-        int age3 = person3.getAge();
-        String name3 = person3.getName();
-        
-        System.out.println("Age1: " + age1);
-        System.out.println("Name1: " + name1);
-        person1.eat();
+        key.close();
 
-        System.out.println("Age2: " + age2);
-        System.out.println("Name2: " + name2);
-        person2.eat();
+        // int age2 = person2.getAge();
+        // String name2 = person2.getName();
 
-        System.out.println("Age3: " + age3);
-        System.out.println("Name3: " + name3);
-        person3.eat();
+        // int age3 = person3.getAge();
+        // String name3 = person3.getName();
+
+        // System.out.println("Age1: " + age1);
+        // System.out.println("Name1: " + name1);
+        // person1.eat();
+
+        // System.out.println("Age2: " + age2);
+        // System.out.println("Name2: " + name2);
+        // person2.eat();
+
+        // System.out.println("Age3: " + age3);
+        // System.out.println("Name3: " + name3);
+        // person3.eat();
     }
 }
