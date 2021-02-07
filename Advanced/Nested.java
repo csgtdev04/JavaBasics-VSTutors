c8e350885cebbcee7fda5e503183ebf4a81749b2
csgtdev04 <saitanujm@gmail.com>
2021-02-07 12:24:17 -0600

    long account logic

--- Advanced/Nested.java
+++ Advanced/Nested.java
@@ -1,7 +1,50 @@
 package Advanced;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Nested {
+
+    public static void createUser() {
+        System.out.println("Creating a new user!");
+    }
+    public static void helpOn() {
+        System.out.println("What do you need help on?");
+    }
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<Long> userAccounts = new ArrayList<Long>();
+        userAccounts.add(235674797L); //l
+        userAccounts.add(362985280L); //l
+        userAccounts.add(637952063L); //l
+        userAccounts.add(237593254L); //l
+
+
+        System.out.println("May I know your account number?");
+        long customer_account_number = input.nextLong();
+        input.nextLine();
+
+        boolean is_user = false;
+
+        for(Long l : userAccounts) {
+            if(l == customer_account_number) {
+                // System.out.println("You are a customer");
+                is_user = true;
+                //Other code
+            }
+        }
+        if(is_user == true) {
+            System.out.println("You are a customer");
+            helpOn();
+        } else {
+            System.out.println("You are not a customer");
+            createUser();
+        }
+        input.close();
+        
         int counter = 0;
         for(int i = 0; i <= 3; i++) {
             for(int j = 0; j <= 2; j++) {
